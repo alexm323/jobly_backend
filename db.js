393432fc61f42566a562dfd0e04c720afd56bcd4
alexm323 <alejandromartinezjr42@gmail.com>
2021-03-14 19:12:34 -0700

    revert change

--- db.js
+++ db.js
@@ -5,18 +5,10 @@
 const { Client } = require("pg");
 const { getDatabaseUri } = require("./config");
 
-// const db = new Client({
-//   connectionString: getDatabaseUri(),
-// });
+const db = new Client({
+  connectionString: getDatabaseUri(),
+});
 
-// db.connect();
-var db = new pg.Client({
-  user: "admin",
-  password: "guest",
-  database: "Jobly",
-  port: 5432,
-  host: "localhost",
-  ssl: true
-}); 
 db.connect();
+
 module.exports = db;
